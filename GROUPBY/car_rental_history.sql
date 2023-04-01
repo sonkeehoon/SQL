4f56ca68e88573e520fa4fcac334af4d83f5e09b
kee_hoon <djfkfk12345@naver.com>
2023-04-01 21:55:30 +0900

    230401

---
+++ GROUPBY/car_rental_history.sql
@@ -0,0 +1,13 @@
+-- 혼자 못풀었고 다른사람 코드를 보고 이해했다
+
+SELECT MONTH(START_DATE) AS month, CAR_ID, COUNT(*) as RECORDS
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY 
+WHERE MONTH(start_date) BETWEEN 8 AND 10
+    AND car_id in (SELECT car_id 
+                   FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+                   WHERE MONTH(start_date) BETWEEN 8 AND 10
+                   GROUP BY car_id
+                   HAVING COUNT(car_id) >= 5)
+GROUP BY month, car_id
+having RECORDS > 0
+ORDER BY month, car_id DESC;
