836fad038f81a102140e0447be424bdf9be040f1
kee_hoon <djfkfk12345@naver.com>
2023-04-02 19:26:55 +0900

    230402

---
+++ GROUPBY/car_rental_company.sql
@@ -0,0 +1,9 @@
+-- 스스로 해결
+-- 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기 : https://school.programmers.co.kr/learn/courses/30/lessons/151137
+SELECT car_type, count(*) as cars
+FROM CAR_RENTAL_COMPANY_CAR
+WHERE FIND_IN_SET('통풍시트', options) 
+    OR FIND_IN_SET('열선시트', options)
+    OR FIND_IN_SET('가죽시트', options)
+GROUP BY car_type
+ORDER BY car_type;
