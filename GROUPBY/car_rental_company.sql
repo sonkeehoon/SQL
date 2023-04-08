973a22a396b267cc195ff4aa5147abba3793e212
kh <81700507+sonkeehoon@users.noreply.github.com>
2023-04-08 17:42:13 +0900

    Update car_rental_company.sql

--- GROUPBY/car_rental_company.sql
+++ GROUPBY/car_rental_company.sql
@@ -1,5 +1,5 @@
--- 스스로 해결
 -- 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기 : https://school.programmers.co.kr/learn/courses/30/lessons/151137
+-- 스스로 해결 여부 : X
 SELECT car_type, count(*) as cars
 FROM CAR_RENTAL_COMPANY_CAR
 WHERE FIND_IN_SET('통풍시트', options) 
